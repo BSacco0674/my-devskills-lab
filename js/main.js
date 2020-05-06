bccb5b5b3a73826f22b2b1ac8a776d2e30c44443
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-05-06 16:00:37 -0500

    toDo_function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ var $toDoList = []
 
 $("#btn").on("click", function(evnt) {
     let toDo = $("#input").val();
+    
+    if (toDo) {
+        var $newItem = $(`<tr>
+        <td class="button"><button>X</button></td>
+        <td class="text">${toDo}</td>
+      </tr>`)
+    }
+    $("#to-dos").append($newItem)
+    $("#input").val("")
