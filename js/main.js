6bcb9c854069685716f0193bd503778e15a91fa4
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-05-06 16:36:30 -0500

    addskill

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-var $toDoList = []
+var $addskill = []
 
 $("#btn").on("click", function(evnt) {
     let toDo = $("#input").val();
     
-    if (toDo) {
+    if (addskil) {
         var $newItem = $(`<tr>
         <td class="button"><button>X</button></td>
         <td class="text">${toDo}</td>
       </tr>`)
     }
-    $("#to-dos").append($newItem)
+    $("#add-skill").append($newItem)
     $("#input").val("")
 
 })
-$("#to-dos").on("click", "button", function() {
+$("#add-skill").on("click", "button", function() {
     $(this).closest('tr').fadeOut(750, function() {
       $(this).remove();
     });
