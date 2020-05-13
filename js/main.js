a48694fa5281bc1914916ebbc42ff251f6325c4d
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-05-12 22:18:13 -0500

    Addedcolorandbuttons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var $addskill = []
 
 $("#btn").on("click", function(evnt) {
-    let toDo = $("#input").val();
+    let addskill = $("#input").val();
     
     if (addskil) {
         var $newItem = $(`<tr>
         <td class="button"><button>X</button></td>
-        <td class="text">${toDo}</td>
+        <td class="text">${addskill}</td>
       </tr>`)
     }
     $("#add-skill").append($newItem)
